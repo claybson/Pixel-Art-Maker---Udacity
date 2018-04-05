0372351ec4e69fea6809bc655731d285df6d3311
Claybson <claybson@cartello.com.br>
2018-04-05 10:49:35 -0300

    Finalização do projeto

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
-// Select color input
-// Select size input
+  //Cancela o evento e chama a função makeGrid()
+  $("input[type='submit']").click(function(e) {
+    e.preventDefault();
+    makeGrid();
+  });
 
-// When size is submitted by the user, call makeGrid()
-
+//Inicio da função
 function makeGrid() {
+    //Declaração de variáveis
+    let cols = $("#inputWeight").val();
+    let rows = $("#inputHeight").val();
+    let canvas = $("#pixelCanvas");
+    const color = $("#colorPicker");
 
-// Your code goes here!
+    //Limpa a tabela
+    canvas.children().remove();
 
-}
+    //Cria a grid através dos dados passados pelo usuário
+    for (let i = 0; i < rows; i++) {
+      canvas.append("<tr></tr>");  
+      for (let j = 0; j < cols; j++){
+        canvas.children().last().append("<td></td>");
+      }
+    }
+
+    //Seleciona a cor, e aplica no quadrado que foi clicado
+    canvas.on("click", "td", function() {
+      let color = $("input[type='color']#colorPicker").val();
+      $(this).attr("bgcolor", color);
+    });
+  }
+
+  
